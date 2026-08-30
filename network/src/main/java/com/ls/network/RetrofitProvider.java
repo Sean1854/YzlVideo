fca8314abb10fe05b3a9d794879640bead54a284
Sean1854 <2849493501@qq.com>
2026-08-30 12:53:25 +0800

    把服务器地址隐藏

--- network/src/main/java/com/ls/network/RetrofitProvider.java
+++ network/src/main/java/com/ls/network/RetrofitProvider.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitProvider {
-    private static final String BASE_URL = "https://titok.fzqq.fun/";
+    private static final String BASE_URL = "隐藏服务器地址";
     private static Retrofit mRetrofit;
     public static Retrofit provider(){
 
